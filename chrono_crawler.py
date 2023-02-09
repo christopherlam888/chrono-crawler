8870230e0c800fd02cb437f4accb6ab462b78785
Christopher Lam <85356197+christopherlam888@users.noreply.github.com>
2023-02-09 18:26:52 +0000

    Fix omegaenthusiast

--- chrono_crawler.py
+++ chrono_crawler.py
@@ -52,14 +52,14 @@ def check_exists(xpath):
 def omegaenthusiast_check_page(omegaenthusiast, counter):
     omegaenthusiast_results = requests.get(omegaenthusiast+str(counter), headers = headers)
     omegaenthusiast_soup = BeautifulSoup(omegaenthusiast_results.text, "html.parser")
-    omegaenthusiast_list = omegaenthusiast_soup.find('ul', class_='S4WbK_ c2Zj9x')
+    omegaenthusiast_list = omegaenthusiast_soup.find('ul', class_='S4WbK_')
     return bool(omegaenthusiast_list.find('li'))
 
 def scrape_omegaenthusiast(page):
     listings_omegaenthusiast = []
     omegaenthusiast_results = requests.get(page, headers = headers)
     omegaenthusiast_soup = BeautifulSoup(omegaenthusiast_results.text, "html.parser")
-    omegaenthusiast_list = omegaenthusiast_soup.find('ul', class_='S4WbK_ c2Zj9x')
+    omegaenthusiast_list = omegaenthusiast_soup.find('ul', class_='S4WbK_')
     omegaenthusiast_products = omegaenthusiast_list.find_all('li')
     for li in omegaenthusiast_products:
         if li.find('span', class_='cfpn1d'):
