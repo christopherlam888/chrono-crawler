67e9f0c760a3a7f211e77c21f3842004b0910a1b
Christopher Lam <85356197+christopherlam888@users.noreply.github.com>
2023-02-11 01:29:15 +0000

    Code cleanup

--- chrono_crawler.py
+++ chrono_crawler.py
@@ -12,8 +12,16 @@ from selenium.common.exceptions import NoSuchElementException
 import multiprocessing
 import tqdm
 import sys
+
 sys.setrecursionlimit(10000)
 import webbrowser
+
+SITES = {
+    "theoandharris": "https://theoandharris.com/vintage-watches/",
+    "delraywatch": "https://www.delraywatch.com/collections/pre-owned-watches",
+    "omegaenthusiast": "https://www.omegaenthusiastltd.com/shop-all?page=",
+}
+
 
 def draw_text():
     print(" ____  ____  ____  ____  ____  ____ ")
@@ -26,169 +34,194 @@ def draw_text():
     print("|/__\||/__\||/__\||/__\||/__\||/__\||/__\|")
     print()
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Available Options")
-    parser.add_argument('-t', '--theoandharris', dest='theoandharris', action='store_true')
-    parser.add_argument('-d', '--delraywatch', dest='delraywatch', action='store_true')
-    parser.add_argument('-o', '--omegaenthusiast', dest='omegaenthusiast', action='store_true')
-    parser.add_argument('-s', '--search', dest='search')
-    parser.add_argument('-p', '--price', dest='price', action='store_true')
+    for key in SITES:
+        parser.add_argument(f"-{key[0]}", f"--{key}", dest=key, action="store_true")
+    parser.add_argument("-s", "--search", dest="search")
+    parser.add_argument("-p", "--price", dest="price", action="store_true")
     args = vars(parser.parse_args())
     return args
+
 
 headers = {"Accept-Language": "en-US, en;q=0.5"}
 options = Options()
 options.headless = True
-options.add_argument('--window-size=1920,1080')
-driver = webdriver.Chrome(options = options)
+options.add_argument("--window-size=1920,1080")
+driver = webdriver.Chrome(options=options)
 
-def check_exists(xpath):
+
+def get_element(xpath):
     try:
-        driver.find_element(By.XPATH, xpath)
+        return driver.find_element(By.XPATH, xpath)
     except NoSuchElementException:
-        return False
-    return True
+        return None
 
-def omegaenthusiast_check_page(omegaenthusiast, counter):
-    omegaenthusiast_results = requests.get(omegaenthusiast+str(counter), headers = headers)
+
+def omegaenthusiast_check_page(counter):
+    omegaenthusiast_results = requests.get(
+        SITES["omegaenthusiast"] + str(counter), headers=headers
+    )
     omegaenthusiast_soup = BeautifulSoup(omegaenthusiast_results.text, "html.parser")
-    omegaenthusiast_list = omegaenthusiast_soup.find('ul', class_='S4WbK_')
-    return bool(omegaenthusiast_list.find('li'))
+    omegaenthusiast_list = omegaenthusiast_soup.find("ul", class_="S4WbK_")
+    return bool(omegaenthusiast_list.find("li"))
+
 
 def scrape_omegaenthusiast(page):
     listings_omegaenthusiast = []
-    omegaenthusiast_results = requests.get(page, headers = headers)
+    omegaenthusiast_results = requests.get(page, headers=headers)
     omegaenthusiast_soup = BeautifulSoup(omegaenthusiast_results.text, "html.parser")
-    omegaenthusiast_list = omegaenthusiast_soup.find('ul', class_='S4WbK_')
-    omegaenthusiast_products = omegaenthusiast_list.find_all('li')
+    omegaenthusiast_list = omegaenthusiast_soup.find("ul", class_="S4WbK_")
+    omegaenthusiast_products = omegaenthusiast_list.find_all("li")
     for li in omegaenthusiast_products:
-        if li.find('span', class_='cfpn1d'):
-            title = li.find('h3', class_='syQOIUy okvj6QX---typography-11-runningText okvj6QX---priority-7-primary syHtuvM FzO_a9').text
-            price = int(li.find('span', class_='cfpn1d').text[1:-3].replace(",",""))
-            photohtml_wrapper = li.find('div', class_='naMHY_ vALCqq')
-            photohtml = photohtml_wrapper.find('img')
-            urlhtml = li.find('a')
-            listings_omegaenthusiast.append(Listing(title, price, photohtml['src'].replace("blur_2,","").replace("w_110,h_107","w_155,h_155"), urlhtml['href'], "Omega Enthusiast"))
+        if li.find("span", class_="cfpn1d"):
+            title = li.find(
+                "h3",
+                class_="syQOIUy okvj6QX---typography-11-runningText okvj6QX---priority-7-primary syHtuvM FzO_a9",
+            ).text
+            price = int(li.find("span", class_="cfpn1d").text[1:-3].replace(",", ""))
+            photohtml_wrapper = li.find("div", class_="naMHY_ vALCqq")
+            photohtml = photohtml_wrapper.find("img")
+            urlhtml = li.find("a")
+            listings_omegaenthusiast.append(
+                Listing(
+                    title,
+                    price,
+                    photohtml["src"]
+                    .replace("blur_2,", "")
+                    .replace("w_110,h_107", "w_155,h_155"),
+                    urlhtml["href"],
+                    "Omega Enthusiast",
+                )
+            )
     return listings_omegaenthusiast
 
-def main():
 
+def main():
     # draw text
     draw_text()
 
-    theoandharris_selected = False
-    delraywatch_selected = False
-    omegaenthusiast_selected = False
-
     # parse arguments
     args = parse_args()
-    if args['theoandharris']:
-        theoandharris_selected = True
-    if args['delraywatch']:
-        delraywatch_selected = True
-    if args['omegaenthusiast']:
-        omegaenthusiast_selected = True
-    if not args['theoandharris'] and not args['delraywatch'] and not args['omegaenthusiast']:
-        theoandharris_selected = True
-        delraywatch_selected = True
-        omegaenthusiast_selected = True
+    all_selected = not any(args[key] for key in SITES)
+    for key in SITES:
+        args[key] = args[key] or all_selected
 
     listings = []
 
     # scrape theoandharris
-    if theoandharris_selected:
+    if args["theoandharris"]:
         print("Scraping Theo and Harris...")
-        theoandharris = "https://theoandharris.com/vintage-watches/"
-        theoandharris_results = requests.get(theoandharris, headers=headers)
+        theoandharris_results = requests.get(SITES["theoandharris"], headers=headers)
         theoandharris_soup = BeautifulSoup(theoandharris_results.text, "html.parser")
-        theoandharris_list = theoandharris_soup.find('ul', class_='columns-4')
-        theoandharris_products = theoandharris_list.find_all('li', class_='product')
+        theoandharris_list = theoandharris_soup.find("ul", class_="columns-4")
+        theoandharris_products = theoandharris_list.find_all("li", class_="product")
         for li in theoandharris_products:
-            title = li.find('h2', class_='woocommerce-loop-product__title').text
-            price = int(li.find('bdi').text[1:-4].replace(",",""))
-            photohtml = li.find('img')
-            urlhtml = li.find('a')
-            listings.append(Listing(title, price, photohtml['src'], urlhtml['href'], "Theo and Harris"))
+            title = li.find("h2", class_="woocommerce-loop-product__title").text
+            price = int(li.find("bdi").text[1:-4].replace(",", ""))
+            photohtml = li.find("img")
+            urlhtml = li.find("a")
+            listings.append(
+                Listing(
+                    title, price, photohtml["src"], urlhtml["href"], "Theo and Harris"
+                )
+            )
         print("Theo and Harris scraped.")
 
     # scrape delraywatch
-    if delraywatch_selected:
+    if args["delraywatch"]:
         print("Scraping Delray Watch...")
-        delraywatch = "https://delraywatch.com/pre-owned-watches/"
-        driver.get(delraywatch)
-        more_pages = True
-        while more_pages:
-            delraywatch_products = driver.find_elements(By.XPATH, "//li[@class='product']")
+        driver.get(SITES["delraywatch"])
+        while True:
+            delraywatch_products = driver.find_elements(
+                By.XPATH, "//li[@class='product']"
+            )
             for li in delraywatch_products:
                 title = li.find_element(By.XPATH, ".//h4[@class='card-title']//a").text
                 if "Inventory" in title:
                     title = title[:-17]
-                price = int(li.find_element(By.XPATH, ".//span[@class='price price--withoutTax']").text[1:-3].replace(",",""))
-                photo = li.find_element(By.TAG_NAME, "img").get_attribute('src')
-                url = li.find_element(By.TAG_NAME, "a").get_attribute('href')
+                price = int(
+                    li.find_element(
+                        By.XPATH, ".//span[@class='price price--withoutTax']"
+                    )
+                    .text[1:-3]
+                    .replace(",", "")
+                )
+                photo = li.find_element(By.TAG_NAME, "img").get_attribute("src")
+                url = li.find_element(By.TAG_NAME, "a").get_attribute("href")
                 listings.append(Listing(title, price, photo, url, "Delray Watch"))
             next_link_xpath = "//li[@class='pagination-item pagination-item--next']//a"
-            if(check_exists(next_link_xpath)):
-                next_link = driver.find_element(By.XPATH, next_link_xpath)
+            if next_link := get_element(next_link_xpath):
                 next_link.click()
                 driver.refresh()
             else:
-                more_pages = False
+                break
         driver.quit()
         print("Delray Watch scraped.")
 
     # scrape omegaenthusiast
-    if omegaenthusiast_selected:
+    if args["omegaenthusiast"]:
         print("Scraping Omega Enthusiast...")
-        omegaenthusiast = "https://www.omegaenthusiastltd.com/shop-all?page="
-        
+
         # check pages
         omegaenthusiast_pages = []
         with multiprocessing.Pool(8) as pool:
             INTERVAL = 8
             counter = 1
             while True:
-                params = [(omegaenthusiast, counter) for counter in range(counter, counter+INTERVAL)]
+                params = [(counter,) for counter in range(counter, counter + INTERVAL)]
                 res = pool.starmap(omegaenthusiast_check_page, params)
                 for i, output in enumerate(res):
                     if output:
-                        omegaenthusiast_pages.append(f"{omegaenthusiast}{counter+i}")
+                        omegaenthusiast_pages.append(
+                            f"{SITES['omegaenthusiast']}{counter+i}"
+                        )
                 if not all(res):
                     break
                 counter += INTERVAL
-        
+
         # scrape pages
-        with multiprocessing.Pool(8) as pool, tqdm.tqdm(total=len(omegaenthusiast_pages)) as pbar:
-            for listings_omegaenthusiast in pool.imap_unordered(scrape_omegaenthusiast, omegaenthusiast_pages):
+        with multiprocessing.Pool(8) as pool, tqdm.tqdm(
+            total=len(omegaenthusiast_pages)
+        ) as pbar:
+            for listings_omegaenthusiast in pool.imap_unordered(
+                scrape_omegaenthusiast, omegaenthusiast_pages
+            ):
                 listings.extend(listings_omegaenthusiast)
                 pbar.update()
         print("Omega Enthusiast scraped.")
 
     # search
-    if args['search']:
-        search = args['search'].strip().lower()
-        listings = list(filter(lambda listing: search in listing.title.lower(), listings))
+    if args["search"]:
+        search = args["search"].strip().lower()
+        listings = [listing for listing in listings if search in listing.title.lower()]
 
     # price sort
-    if args['price']:
-        listings.sort(key=lambda listing: listing.price)
+    if args["price"]:
+        listings.sort()
 
     # print table
     print()
     print("{:<5} {:<120} {:<10} {:<20}".format("Item", "Title", "Price", "Store"))
-    for i in range(len(listings)):
-        print("{:<5} {:<120} {:<10} {:<20}".format(i, listings[i].title, "$" + '{:,}'.format(listings[i].price), listings[i].store))
+    for i, listing in enumerate(listings):
+        print(
+            "{:<5} {:<120} {:<10} {:<20}".format(
+                i, listing.title, f"${listing.price:,}", listing.store
+            )
+        )
 
     # browsing
-    browsing = True
-    while browsing:
-        selection = input("Browsing...Enter an item number: ")
-        if selection.isdigit() and int(selection) >= 0 and int(selection) < len(listings):
-            print(f"Photo:\n{listings[int(selection)].photo}")
-            print(f"URL:\n{listings[int(selection)].url}")
-            webbrowser.open(listings[int(selection)].url, new=2)
-        else:
-            browsing = False
+    while True:
+        try:
+            selection = int(input("Browsing...Enter an item number: "))
+            if 0 <= selection < len(listings):
+                print(f"Photo:\n{listings[selection].photo}")
+                print(f"URL:\n{listings[selection].url}")
+                webbrowser.open(listings[selection].url, new=2)
+        except ValueError:
+            break
+
 
 if __name__ == "__main__":
     main()
--- listing.py
+++ listing.py
@@ -1,7 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass(eq=True)
 class Listing:
-    def __init__(self, title, price, photo, url, store):
-        self.title = title
-        self.price = price
-        self.photo = photo
-        self.url = url
-        self.store = store
+    title: str
+    price: int
+    photo: str
+    url: str
+    store: str
+
+    def __lt__(self, other):
+        return self.price <= other.price
